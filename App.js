b6372289d752514b791712a6451bce96881f9734
Nikhil <you@example.com>
2024-02-25 13:21:22 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement(
-      "div",
-      { id: "child" },
-      [React.createElement("h1", { id: "h1" }, "I'm an h1 tag"),React.createElement("h2", { id: "h2" }, "I'm an h2 tag")]
-    ), React.createElement(
-      "div",
-      { id: "child2" },
-      [React.createElement("h1", { id: "h1" }, "I'm an h1 tag"),React.createElement("h2", { id: "h2" }, "I'm an h2 tag")]
-    )]
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./logo.png";
+import userIcon from "./userIcon.png";
+import "./Assignment.css";
+
+// const Title = () => <h1 className="head">Namaste React !!!</h1>;
+
+// const HeadingComponent = () => (
+//   <div id="container">
+//     <Title />
+//     <h1 className="heading">This is functional component in jSX</h1>
+//   </div>
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingComponent />);
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="left">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="center">
+          <input
+            className="input"
+            type="text"
+            placeholder="Search anything you want..."
+          />
+          <button type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
+        <div className="right">
+          <img src={userIcon} alt="User Icon" />
+        </div>
+      </header>
+    </>
   );
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(parent);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
